76f4edd5b95e25ed6cb4374d4840078bf6351753
Karin Tsai <karin@joinpropel.com>
2023-12-04 22:41:55 -0500

    Simplify Day 4 2023

--- 2023/4.py
+++ 2023/4.py
@@ -6,39 +6,29 @@ from collections import defaultdict
 from util import run
 
 
-def parse_input(line, callback):
+def get_num_matches(line):
     num_strs = line.split(': ')[1]
     winning, card = num_strs.split(' | ')
     winning = set(re.findall('(\d+)', winning))
     card = set(re.findall('(\d+)', card))
-    callback(winning, card)
+    return len(winning & card)
 
 
 def part_1(lines):
     answer = 0
-    
-    def callback(winning, card):
-        overlap = len(winning & card)
-        answer += (1 if overlap else 0) * math.pow(2, overlap - 1)
-
     for line in lines:
-        parse_input(line, callback)
-        
+        num_matches = get_num_matches(line)
+        answer += (1 if num_matches else 0) * math.pow(2, num_matches - 1)
     return answer
 
 
 def part_2(lines):
     answer = 0
     copy_counts = defaultdict(int)
-
     for i, line in enumerate(lines):
-        def callback(winning, card):
-            matching = len(winning & card)
-            for j in range(matching):
-                copy_counts[i + j + 1] += 1 + copy_counts[i]
-
-        parse_input(line, callback)
-
+        num_matches = get_num_matches(line)
+        for j in range(num_matches):
+            copy_counts[i + j + 1] += 1 + copy_counts[i]
     return sum(copy_counts.values()) + len(lines)
 
 
