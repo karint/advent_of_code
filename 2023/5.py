0e5b416e8619e8918936aa9e3d9c10d94a1cc562
Karin Tsai <karin@joinpropel.com>
2023-12-06 01:37:03 -0500

    Add Day 5 2023 Part 2

--- 2023/5.py
+++ 2023/5.py
@@ -1,13 +1,10 @@
 import os
-import json
 import re
 
-from collections import defaultdict
 from util import run
 
 
 def part_1(lines):
-    answer = 0
     seeds = None
     names = []
     map_dicts = []
@@ -19,6 +16,7 @@ def part_1(lines):
             seeds = map(int, line.split(': ')[1].split(' '))
             continue
         elif ':' in line:
+            # Tracking names is mostly for debugging and not required in the solution
             names.append(line.split(' ')[0])
             map_dicts.append({})
             continue
@@ -29,15 +27,12 @@ def part_1(lines):
         map_dicts[-1][(
             source_start,
             source_start + range_length
-        )] = (
-            dest_start,
-            dest_start + range_length
-        )
+        )] = dest_start
 
     min_location = None
     for seed in seeds:
         for name, mapping in zip(names, map_dicts):
-            for (source_start, source_end), (dest_start, dest_end) in mapping.items():
+            for (source_start, source_end), dest_start in mapping.items():
                 if source_start <= seed and seed < source_end:
                     seed = dest_start + seed - source_start
                     break
@@ -48,8 +43,87 @@ def part_1(lines):
     return min_location
 
 
+def map_ranges_to_destination(ranges, mapping):
+    """
+    Takes source ranges and a source-to-destination mapping and returns
+    the new set of ranges at the destination.
+    """
+    new_ranges = set()
+    for start, end in ranges:
+        covered_ranges = []
+        for (source_start, source_end), dest_start in mapping.items():
+            min_overlap = max(start, source_start)
+            max_overlap = min(end, source_end)
+
+            # There's overlap -- map the new segments to their destination
+            if min_overlap < max_overlap:
+                diff = dest_start - source_start
+                new_range = (min_overlap + diff, max_overlap + diff)
+                new_ranges.add(new_range)
+                covered_ranges.append((min_overlap, max_overlap))
+
+        if not covered_ranges:
+            # No overlap in any mappings -- keep original range
+            new_ranges.add((start, end))
+        else:
+            # Add what remains of the original range that wasn't covered by the mapping
+            covered_ranges.sort()
+            if start < covered_ranges[0][0]:
+                new_ranges.add((start, covered_ranges[0][0]))
+            for i, (covered_start, covered_end) in enumerate(covered_ranges):
+                if i == len(covered_ranges) - 1:
+                    break
+                next_start, _ = covered_ranges[i + 1]
+                if next_start > covered_end:
+                    new_ranges.add((covered_end, next_start))
+            if end > covered_ranges[-1][1]:
+                new_ranges.add((covered_ranges[-1][1], end))
+    return new_ranges
+
+
 def part_2(lines):
-    part_1(lines)
+    # Parse all the things!
+    seed_ranges = []
+    names = []
+    map_dicts = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        elif line.startswith('seeds'):
+            starts_and_ranges = re.findall('(\d+ \d+)', line.split(': ')[1])
+            for string in starts_and_ranges:
+                start, range_ = map(int, string.split(' '))
+                seed_ranges.append((start, start + range_))
+            continue
+        elif ':' in line:
+            # Tracking names is mostly for debugging and not required in the solution
+            names.append(line.split(' ')[0])
+            map_dicts.append({})
+            continue
+
+        dest_start, source_start, range_length = map(
+            int, re.findall('(\d+)', line)
+        )
+        map_dicts[-1][(
+            source_start,
+            source_start + range_length
+        )] = dest_start
+
+    # Approach will be to track the spans still viable from initial seed ranges
+    # at each source category. At the end, the lowest viable location span will
+    # contain the minimum location as it's range start.
+    possible_span_value_ranges = set(seed_ranges)
+
+    for name, curr_map in zip(names, map_dicts):
+        new_ranges = map_ranges_to_destination(possible_span_value_ranges, curr_map)
+        possible_span_value_ranges = new_ranges
+
+    min_location = None
+    for start, _ in possible_span_value_ranges:
+        if min_location is None or start < min_location:
+            min_location = start
+    return min_location
 
 
 if __name__ == '__main__':
