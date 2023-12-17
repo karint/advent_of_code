606342dff9ebe07ef191e5d5b16ea29968f55dea
Karin Tsai <karin@joinpropel.com>
2023-12-17 02:04:25 -0500

    Clean up Day 17 2023

--- 2023/17.py
+++ 2023/17.py
@@ -1,6 +1,6 @@
 """
-Part 1:
-Part 2:
+Part 1: Move a crucible through a grid while minimizing heat loss.
+Part 2: Move a bigger and needier crucible.
 """
 import os
 
@@ -13,31 +13,23 @@ OPPOSITES ={
     Direction.DOWN: Direction.UP,
 }
 
-SYMBOL = {
-    Direction.RIGHT: '>',
-    Direction.UP: '^',
-    Direction.LEFT: '<',
-    Direction.DOWN: 'v',
-}
-
-
-class PathGrid(object):
-    """
-    Class that can find the shortest path from a starting char to ending char in a grid.
-
-    Warning: Not very efficient :)
-    """
+class Grid(object):
     def __init__(
         self,
-        lines
+        lines,
+        min_steps,
+        max_steps,
     ):
         self.grid = [list(map(int, l.strip())) for l in lines]
 
         self.width = len(self.grid[0])
         self.height = len(self.grid)
 
-        (self.start_x, self.start_y) = (0, 0)
-        self.ending_point = (self.width - 1, self.height - 1)
+        self.start_x, self.start_y = 0, 0
+        self.end_x, self.end_y = self.width - 1, self.height - 1
+
+        self.min_steps = min_steps
+        self.max_steps = max_steps
 
     def find_paths(self):
         min_heat = None
@@ -48,45 +40,44 @@ class PathGrid(object):
         ])
         while viable_paths:
             new_paths = set()
-            for (last_x, last_y, curr_dir, steps_so_far, heat_so_far) in viable_paths:
-                if (last_x, last_y) == self.ending_point and steps_so_far >= 4:
+            for last_x, last_y, curr_dir, steps_so_far, heat_so_far in viable_paths:
+                # If we meet ending conditions, store heat if it's min
+                if (last_x, last_y) == (self.end_x, self.end_y) and steps_so_far >= self.min_steps:
                     if min_heat is None or heat_so_far < min_heat:
                         min_heat = heat_so_far
                         continue
 
+                # If we're already too hot, no point in continuing
                 if min_heat is not None and heat_so_far >= min_heat:
                     continue
 
-                if (last_x, last_y, curr_dir, steps_so_far) in visited:
-                    if heat_so_far >= visited[(last_x, last_y, curr_dir, steps_so_far)]:
+                # If we took a path before that was not as hot, no point in continuing
+                if (
+                    (last_x, last_y, curr_dir, steps_so_far) in visited and
+                    heat_so_far >= visited[(last_x, last_y, curr_dir, steps_so_far)]
+                ):
                         continue
-                    else:
-                        visited[(last_x, last_y, curr_dir, steps_so_far)] = heat_so_far
                 else:
                     visited[(last_x, last_y, curr_dir, steps_so_far)] = heat_so_far
 
-                right = (Direction.RIGHT, last_x + 1, last_y)
-                left = (Direction.LEFT, last_x - 1, last_y)
-                up = (Direction.UP, last_x, last_y - 1)
-                down = (Direction.DOWN, last_x, last_y + 1)
-
-                for direction, target_x, target_y in (right, left, up, down):
-                    # Don't revisit past squares, go out of bounds, or go too far in one direction
+                for direction, target_x, target_y in (
+                    (Direction.RIGHT, last_x + 1, last_y),
+                    (Direction.DOWN, last_x, last_y + 1),
+                    (Direction.LEFT, last_x - 1, last_y),
+                    (Direction.UP, last_x, last_y - 1),
+                ):
+                    # Don't revisit go out of bounds, past squares, or go too far in one direction
                     if (
-                        target_x < 0 or
-                        target_x >= self.width or
-                        target_y < 0 or
-                        target_y >= self.height
+                        # Don't go out of bounds
+                        target_x < 0 or target_x >= self.width or
+                        target_y < 0 or target_y >= self.height or
+                        # Or take too few steps
+                        (direction != curr_dir and steps_so_far < self.min_steps) or
+                        # Or take too many steps
+                        (direction == curr_dir and steps_so_far >= self.max_steps) or
+                        # or go backwards
+                        direction == OPPOSITES[curr_dir]
                     ):
-                        continue
-
-                    if direction != curr_dir and steps_so_far < 4:
-                        continue
-
-                    if direction == curr_dir and steps_so_far >= 10:
-                        continue
-
-                    if direction == OPPOSITES[curr_dir]:
                         continue
 
                     new_step = (
@@ -100,26 +91,15 @@ class PathGrid(object):
 
             viable_paths = new_paths
 
-        # path_key = {
-        #     (x, y): SYMBOL[d] for (x, y, d, _, _) in min_path
-        # }
-        # for y, row in enumerate(self.grid):
-        #     line = ''
-        #     for x, char in enumerate(row):
-        #         line += path_key.get((x, y), str(char))
-        #     print(line)
-
         return min_heat
 
 
 def part_1(lines):
-    grid = PathGrid(lines)
-    min_heat = grid.find_paths()
-    return min_heat
+    return Grid(lines, 0, 3).find_paths()
 
 
 def part_2(lines):
-    return part_1(lines)
+    return Grid(lines, 4, 10).find_paths()
 
 
 if __name__ == '__main__':
