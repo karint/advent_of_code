0ba0cf0ceb6328c43df9147dced8e7827e7545fd
Karin Tsai <karin@joinpropel.com>
2023-12-18 03:05:29 -0500

    Move polygon area method into util

--- 2023/18.py
+++ 2023/18.py
@@ -6,7 +6,7 @@ import math
 import os
 import sys
 
-from util import Direction, run
+from util import Direction, get_polygon_area, run
 
 DIR_MAP = {
     '0': 'R',
@@ -14,6 +14,7 @@ DIR_MAP = {
     '2': 'L',
     '3': 'U',
 }
+
 
 def move(direction, x, y, steps=1):
     if direction == 'R':
@@ -26,30 +27,15 @@ def move(direction, x, y, steps=1):
         return (x, y - steps)
 
 
-def shoelace(vertices):
-    # The Shoelace Algorithm - www.101computing.net/the-shoelace-algorithm
-    total = 0
-    for i, (x, y) in enumerate(vertices):
-        if i == len(vertices) - 1:
-            continue
-        next_vertex = vertices[i + 1]
-        total += x * next_vertex[1] - y * next_vertex[0]
-
-    total += vertices[-1][0] * vertices[0][1] - vertices[-1][1] * vertices[0][0]
-    return abs(total) / 2
-
-
 def find_trench_area(lines, parse_ln_fn):
     trench_lines = []
-    perimeter = 0
     curr_x, curr_y = 0, 0
     for line in lines:
         line = line.strip()
         direction, num_steps = parse_ln_fn(line)
-        perimeter += num_steps
         curr_x, curr_y = move(direction, curr_x, curr_y, steps=num_steps)
         trench_lines.append((curr_x, curr_y))
-    return int(shoelace(trench_lines) + perimeter/2 + 1)
+    return get_polygon_area(trench_lines)
 
 
 def part_1(lines):
--- 2023/util.py
+++ 2023/util.py
@@ -73,6 +73,34 @@ def get_manhattan_distance(coord_1, coord_2):
     return dx + dy
 
 
+def get_polygon_area(vertices, include_perimeter=True):
+    """
+    Shoelace algorithm.
+    """
+    total = 0
+    perimeter = 0
+    for i, (x, y) in enumerate(vertices):
+        if i == len(vertices) - 1:
+            continue
+        next_vertex = vertices[i + 1]
+        total += x * next_vertex[1] - y * next_vertex[0]
+        if include_perimeter:
+            perimeter += abs(next_vertex[0] - x) + abs(next_vertex[1] - y)
+
+    last_vertex = vertices[-1]
+    first_vertex = vertices[0]
+    total += last_vertex[0] * first_vertex[1] - last_vertex[1] * first_vertex[0]
+    area = abs(total) / 2
+    if include_perimeter:
+        perimeter += (
+            abs(last_vertex[0] - first_vertex[0]) +
+            abs(last_vertex[1] - first_vertex[1])
+        )
+        area += perimeter/2 + 1
+    return int(area)
+
+
+
 def find_digits(line, cast_to=int):
     return list(map(cast_to, re.findall('(-?\d+)', line.strip())))
 
