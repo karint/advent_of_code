62ba4f24dd5a0063d794dc4f7a4896c9756e4f79
Karin Tsai <karin@joinpropel.com>
2023-12-21 23:45:55 -0500

    Formatting nitpicks

--- 2023/21.py
+++ 2023/21.py
@@ -65,12 +65,9 @@ def part_2(lines):
     grid, start_x, start_y = parse_grid(lines)
     width = len(grid)
 
-    '''
-    Keep track in initial starting grid which tiles are even and which are odd. The odd tiles are
-    valid ending spots if # of steps is odd, which is the case here. If we take width / 2 steps,
-    we fill the inner diamond. While we're at it, I'd also like to find the total even/odd places
-    of the space outside of the diamond.
-    '''
+    # Keep track in initial starting grid which tiles are even and which are odd. The odd tiles are
+    # valid ending spots if # of steps is odd. If we take width / 2 steps, we fill the inner
+    # diamond. While we're at it, find the total even/odd places of the space outside the diamond.
     walked = set()
     parity_sets = defaultdict(set)
     even = parity_sets[(DiamondType.INNER, Parity.EVEN)]
@@ -90,7 +87,6 @@ def part_2(lines):
                     new_starts.add((nx, ny))
 
         num_steps += 1
-
         if num_steps > width // 2:
             even = parity_sets[(DiamondType.OUTER, Parity.EVEN)]
             odd = parity_sets[(DiamondType.OUTER, Parity.ODD)]
@@ -100,12 +96,11 @@ def part_2(lines):
     parity_counts = {key: len(coords) for key, coords in parity_sets.items()}
 
     """
-    With each additional `width` steps beyond the original starting point, we create additional diamonds.
-    Each diamond adjacent to the inner diamond flips its even/odd parity. Outer diamonds always come in
-    equal numbers of odd/even parities, so we don't worry about flipping them.
+    With each additional `width` steps beyond the original starting point, we create additional
+    diamonds. Each diamond adjacent to the inner diamond flips its even/odd parity. Outer diamonds always come in equal numbers of odd/even parities, so we don't worry about flipping them.
 
-    For example, if we are taking 26501365 steps, after subtracting out the original 65 to get to the
-    edge of the diamond, we get (26501365 - 65)/131 = 202300 expansions of the inner diamond.
+    For example, if we are taking 26501365 steps, after subtracting out the original 65 to get to
+    the edge of the diamond, we get (26501365 - 65)/131 = 202300 expansions of the inner diamond.
 
     Now we just add the proper even or odd values as we expand out the diamond.
     """
@@ -115,21 +110,26 @@ def part_2(lines):
     last_root = 1
     for step in range(expansions + 1):
         step_is_even = step % 2 == 0
+
         # Square root of the number of diamonds
         root = step * 2 + 1
+
         # Get the outer blocks, which are the new values to add
         outer_blocks = math.pow(root, 2) - last_root
-        # Add the copies of the inner diamond, which will be half of the outer blocks. If we're on an even expansion,
-        # we'll want to add the count of the same parity as the number of steps.
+
+        # Add the copies of the inner diamond, which will be half of the outer blocks.
+        # If we're on an even expansion, we'll want to add the count of the same parity
+        # as the number of steps.
         value += outer_blocks // 2 * (
             parity_counts[(DiamondType.INNER, Parity.EVEN)] if even_is_good == step_is_even
             else parity_counts[(DiamondType.INNER, Parity.ODD)]
         )
+
         if step > 0:
             # Other half of the outer blocks are equal numbers of even and odd outer diamonds
             value += outer_blocks // 4 * (
-                parity_counts[(DiamondType.OUTER, Parity.ODD)] +
-                parity_counts[(DiamondType.OUTER, Parity.EVEN)]
+                parity_counts[(DiamondType.OUTER, Parity.EVEN)] +
+                parity_counts[(DiamondType.OUTER, Parity.ODD)]
             )
         last_root = math.pow(root, 2)
 
