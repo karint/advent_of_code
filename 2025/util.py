671f324f78cdb5037a63d48f34adc81fb77b58e8
Karin Tsai <ktsai11@gmail.com>
2025-11-26 14:33:30 -0500

    Set up 2025 folder

---
+++ 2025/util.py
@@ -0,0 +1,7 @@
+"""
+Imports all utils from the utils sibling directory, allowing us to easily
+still import them with this directory as the PYTHONPATH.
+"""
+import sys
+sys.path.append('..')
+from utils import *
