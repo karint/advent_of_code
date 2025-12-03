2168d07fe88a23f3eb925491681b97b41bffc287
Karin Tsai <ktsai11@gmail.com>
2025-12-03 00:49:21 -0500

    Clarify variable names for Day 3 2025

--- 2025/03.py
+++ 2025/03.py
@@ -6,18 +6,18 @@ import os
 
 from util import run
 
-DIGITS = '9876543210'
+DIGIT_DESC_ORDER = '9876543210'
 
 
-def max_joltage(line, digits=12):
-    if digits == 1:
+def max_joltage(line, digits_left=12):
+    if digits_left == 1:
         return max(int(d) for d in line)
 
-    for digit in DIGITS:
+    for digit in DIGIT_DESC_ORDER:
         try:
             num_index = line.index(digit)
-            if 0 <= num_index <= len(line) - digits:
-                return int(line[num_index] + str(max_joltage(line[num_index + 1:], digits=digits-1)))
+            if 0 <= num_index <= len(line) - digits_left:
+                return int(line[num_index] + str(max_joltage(line[num_index + 1:], digits_left=digits_left-1)))
         except ValueError:
             continue
 
@@ -25,11 +25,11 @@ def max_joltage(line, digits=12):
 
 
 def part_1(lines):
-    return sum(max_joltage(line.strip(), digits=2) for line in lines)
+    return sum(max_joltage(line.strip(), digits_left=2) for line in lines)
 
 
 def part_2(lines):
-    return sum(max_joltage(line.strip(), digits=12) for line in lines)
+    return sum(max_joltage(line.strip(), digits_left=12) for line in lines)
 
 
 if __name__ == '__main__':
