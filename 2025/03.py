28f7d2ca3d2562305b5bdccdd00aac6a19ce9ad6
Karin Tsai <ktsai11@gmail.com>
2025-12-03 00:56:05 -0500

    Slightly improve performance of Day 3 2025

--- 2025/03.py
+++ 2025/03.py
@@ -11,25 +11,26 @@ DIGIT_DESC_ORDER = '9876543210'
 
 def max_joltage(line, digits_left=12):
     if digits_left == 1:
-        return max(int(d) for d in line)
+        return max(d for d in line)
 
+    line_length = len(line)
     for digit in DIGIT_DESC_ORDER:
         try:
             num_index = line.index(digit)
-            if 0 <= num_index <= len(line) - digits_left:
-                return int(line[num_index] + str(max_joltage(line[num_index + 1:], digits_left=digits_left-1)))
+            if num_index <= line_length - digits_left:
+                return line[num_index] + max_joltage(line[num_index + 1:], digits_left=digits_left-1)
         except ValueError:
             continue
 
-    return int(line)
+    return line
 
 
 def part_1(lines):
-    return sum(max_joltage(line.strip(), digits_left=2) for line in lines)
+    return sum(int(max_joltage(line.strip(), digits_left=2)) for line in lines)
 
 
 def part_2(lines):
-    return sum(max_joltage(line.strip(), digits_left=12) for line in lines)
+    return sum(int(max_joltage(line.strip(), digits_left=12)) for line in lines)
 
 
 if __name__ == '__main__':
