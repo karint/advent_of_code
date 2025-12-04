1b0badf16c27520362237d42461d3c3c3006715e
Karin Tsai <ktsai11@gmail.com>
2025-12-04 00:39:32 -0500

    Clean up Day 4 2025

--- 2025/04.py
+++ 2025/04.py
@@ -1,78 +1,61 @@
 """
-Part 1: 
-Part 2: 
+Part 1: Find how many rolls of paper have fewer than 4 adjacent rolls of paper.
+Part 2: Find how many rolls of paper you can remove if you iteratively remove all
+        paper with fewer than 4 adjacent rolls.
 """
 import os
 
-from util import run, get_adjacent_coords
+from util import get_adjacent_coords, run
+
+PAPER_CHAR = '@'
+MOVABLE_MAX_ADJ_ROLLS = 4
 
 
-def part_1(lines):
-    grid = []
-    papers = set()
-    empty = set()
-    for y, row in enumerate(lines):
-        grid.append(row)
-        for x, char in enumerate(row):
-            if char == '@':
-                papers.add((x, y))
-            else:
-                empty.add((x, y))
-
-    count = 0
-    good = set()
+def _print_grid(lines, paper_coords):
     for y, row in enumerate(lines):
         s = ''
         for x, char in enumerate(row):
-            if char == '@':
-                adj = get_adjacent_coords(x, y)
-                if (
-                    sum(1 if (x2, y2) in papers else 0 for x2, y2 in adj) < 4
-                ):
-                    count += 1
-                    s += 'x'
-                else:
-                    s += '@'
-            else:
-                s += '.'
+            s += PAPER_CHAR if (x, y) in paper_coords else '.'
+        print(s)
+
+
+def _get_paper_coords(lines):
+    return {
+        (x, y)
+        for y, row in enumerate(lines)
+        for x, char in enumerate(row)
+        if char == PAPER_CHAR
+    }
+
+
+def part_1(lines):
+    paper_coords = _get_paper_coords(lines)
+
+    count = 0
+    for x, y in paper_coords:
+        adjacent_coords = get_adjacent_coords(x, y)
+        if sum(1 if (adj_x, adj_y) in paper_coords else 0 for adj_x, adj_y in adjacent_coords) < MOVABLE_MAX_ADJ_ROLLS:
+            count += 1
 
     return count
 
 
 def part_2(lines):
-    grid = []
-    empty = set()
-    papers = set()
-    for y, row in enumerate(lines):
-        grid.append(row)
-        for x, char in enumerate(row):
-            if char == '@':
-                papers.add((x, y))
-            else:
-                empty.add((x, y))
+    paper_coords = _get_paper_coords(lines)
 
     count = 0
-    removed = None
-    while removed is None or removed:
-        removed = False
-        for y, row in enumerate(lines):
-            s = ''
-            for x, char in enumerate(row):
-                if (x, y) in papers:
-                    adj = get_adjacent_coords(x, y)
-                    if (
-                        sum(1 if (x2, y2) in papers else 0 for x2, y2 in adj) < 4
-                    ):
-                        count += 1
-                        removed = True
-                        papers.remove((x, y))
-                        s += 'x'
-                    else:
-                        s += '@'
-                else:
-                    s += '.'
-            print(s)
+    while True:
+        new_paper_coords = set()
+        for x, y in paper_coords:
+            adjacent_coords = get_adjacent_coords(x, y)
+            if sum(1 if (adj_x, adj_y) in paper_coords else 0 for adj_x, adj_y in adjacent_coords) < MOVABLE_MAX_ADJ_ROLLS:
+                count += 1
+            else:
+                new_paper_coords.add((x, y))
+        if len(new_paper_coords) == len(paper_coords):
+            break
 
+        paper_coords = new_paper_coords
 
     return count
 
