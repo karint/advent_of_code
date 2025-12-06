9a076eb5114363edb5947c0e2c12ab1bd42b7076
Karin Tsai <ktsai11@gmail.com>
2025-12-05 20:19:56 -0500

    Clean up Day 5 2025

--- 2025/05.py
+++ 2025/05.py
@@ -2,8 +2,8 @@
 Started with 06:54:00 left to Day 6 (5:06pm ET).
 Took break between Part 1 and Part 2 (daycare pickup and such!).
 
-Part 1: 
-Part 2: 
+Part 1: Find how many available IDs fell within the given ranges.
+Part 2: Find how many total numbers fell within the given ranges.
 """
 import os
 
@@ -14,18 +14,12 @@ def part_1(lines):
 	count = 0
 	fresh = set()
 
-	is_fresh = True
 	for line in lines:
 		line = line.strip()
-
-		if not line:
-			is_fresh = False
-			continue
-
-		if is_fresh:
+		if '-' in line:
 			start, end = line.split('-')
 			fresh.add((int(start), int(end)))
-		else:
+		elif line:
 			avail = int(line)
 			if any(start <= avail <= end for start, end in fresh):
 				count += 1
@@ -34,19 +28,14 @@ def part_1(lines):
 
 
 def part_2(lines):
-	count = 0
 	fresh_ranges = []
-
 	for line in lines:
 		line = line.strip()
 		if not line:
 			break
 
 		start, end = line.split('-')
-		start = int(start)
-		end = int(end)
-		fresh_ranges.append([start, end])
-
+		fresh_ranges.append([int(start), int(end)])
 	fresh_ranges.sort()
 
 	for i, range_ in enumerate(fresh_ranges):
@@ -54,23 +43,13 @@ def part_2(lines):
 		if i + 1 == len(fresh_ranges):
 			break
 
-		other_start, other_end = fresh_ranges[i + 1]
-
-		# start should be <= other_start due to sorting
-		if start == other_start:
-			# Can delete this range since encapsulated by the next
+		next_start, next_end = fresh_ranges[i + 1]
+		if end >= next_start:
 			range_.clear()
-		elif end < other_start:
-			# Keep as-is -- no overlap
-			pass
-		elif end >= other_start:
-			# Merge ranges
-			range_.clear()
-			fresh_ranges[i + 1][0] = min(start, other_start)
-			fresh_ranges[i + 1][1] = max(end, other_end)
-		else:
-			print('This should not be possible!')
+			fresh_ranges[i + 1][0] = min(start, next_start)
+			fresh_ranges[i + 1][1] = max(end, next_end)
 
+	count = 0
 	for range_ in fresh_ranges:
 		if range_:
 			start, end = range_
