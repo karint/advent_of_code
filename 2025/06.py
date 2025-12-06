9df1a2296f277b1badd5328c1888302bc1d35153
Karin Tsai <ktsai11@gmail.com>
2025-12-06 01:04:49 -0500

    Clean up Day 6 2025

--- 2025/06.py
+++ 2025/06.py
@@ -1,87 +1,65 @@
 """
-Part 1: 
-Part 2: 
+Part 1: Do math problems down each column separated by spaces.
+Part 2: Do math problems right-to-left where each number is in a col.
 """
 import os
 
-from util import find_digits, run
+from math import prod
+from util import run
 
 
 def part_1(lines):
-	count = 0
 	problems = []
-	len_ = None
-
 	for line in lines:
-		line = line.strip()
-		parts = line.split()
-		len_ = len(parts)
-		problems.append(parts)
+		parts = line.strip().split()
+		if not problems:
+			problems = [[] for _ in range(len(parts))]
 
-	for i in range(len_):
-		op = problems[len(problems) - 1][i]
-		if op == '+':
-			count += sum(int(problems[j][i]) for j in range(len(problems) - 1))
-		else:
-			prod = 1
-			for j in range(len(problems) - 1):
-				prod *= int(problems[j][i])
-			count += prod
+		for i, part in enumerate(parts):
+			problems[i].append(part)
+
+	count = 0
+	for problem in problems:
+		numbers = map(int, problem[:-1])
+		op = sum if problem[len(problem) - 1] == '+' else prod
+		count += op(numbers)
 
 	return count
 
 
 def part_2(lines):
-	count = 0
 	max_len = 0
-	cols = []
-	ops = []
-
+	rows = []
 	for line in lines:
 		line = line.rstrip()
 		max_len = max(max_len, len(line))
-		cols.append(line)
+		rows.append(line)
 
+	count = 0
 	op = None
 	nums = []
-	for i in range(max_len):
-		num_index = max_len - i - 1
+	for num_index in reversed(range(max_len)):
 		num_str = ''
-		for col in cols:
-			if len(col) <= num_index:
-				continue
 
-			char = col[num_index]
-			if char in ('+', '*'):
-				op = char
-			else:
-				num_str += char
+		# Check each row to see if there's a character in the given col,
+		# starting from the right
+		for row in rows:
+			if len(row) > num_index:
+				char = row[num_index]
+				if char in ('+', '*'):
+					op = sum if char == '+' else prod
+				else:
+					num_str += char
 
 		num_str = num_str.strip()
 
-		# If whole string is spaces, move to next problem
 		if num_str:
 			nums.append(int(num_str))
 		else:
-			if op == '+':
-				count += sum(nums)
-			else:
-				prod = 1
-				for num in nums:
-					prod *= num
-				count += prod
+			count += op(nums)
+			nums.clear()
 
-			nums = []
-			op = None
-
-
-	if op == '+':
-		count += sum(nums)
-	else:
-		prod = 1
-		for num in nums:
-			prod *= num
-		count += prod
+	count += op(nums)
 	return count
 
 
