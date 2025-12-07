53d8d491449fb8c30596128bed95cc06c2f1a79f
Karin Tsai <ktsai11@gmail.com>
2025-12-07 01:26:39 -0500

    Add initial Day 7 2025 solution

---
+++ 2025/07.py
@@ -0,0 +1,107 @@
+"""
+Part 1: 
+Part 2: 
+"""
+import os
+
+from collections import defaultdict
+from util import run
+
+
+def part_1(lines):
+    split_count = 0
+    grid = []
+    start = None
+    splitters = set()
+
+    width = len(lines[0])
+    height = len(lines)
+
+    def is_in_grid(x, y):
+        return 0 <= x < width and 0 <= y < height
+
+    for y, row in enumerate(lines):
+        grid.append(row.strip());
+        for x, char in enumerate(row):
+            if char == 'S':
+                start = (x, y)
+            elif char == '^':
+                splitters.add((x, y))
+
+    beams = set()
+    beams.add(start)
+    while beams:
+        new_beams = set()
+        for bx, by in beams:
+            split = False
+            down = (bx, by + 1)
+            if down in splitters:
+                left = (bx - 1, by + 1)
+                right = (bx + 1, by + 1)
+                split = False
+                if is_in_grid(*left):
+                    new_beams.add(left)
+                    split = True
+                if is_in_grid(*right):
+                    new_beams.add(right)
+                    split = True
+
+                if split:
+                    split_count += 1
+            elif is_in_grid(*down):
+                new_beams.add(down)
+        beams = new_beams
+
+    return split_count
+
+
+def part_2(lines):
+    path_count = 0
+    grid = []
+    start = None
+    splitters = set()
+
+    width = len(lines[0])
+    height = len(lines)
+
+    def is_in_grid(x, y):
+        return 0 <= x < width and 0 <= y < height
+
+    for y, row in enumerate(lines):
+        grid.append(row.strip());
+        for x, char in enumerate(row):
+            if char == 'S':
+                start = (x, y)
+            elif char == '^':
+                splitters.add((x, y))
+
+    beams = set()
+    beams.add(start)
+    beam_dimensions = defaultdict(int)
+    beam_dimensions[start] += 1
+    while beams:
+        new_beams = set()
+        for bx, by in beams:
+            down = (bx, by + 1)
+            if down in splitters:
+                left = (bx - 1, by + 1)
+                right = (bx + 1, by + 1)
+                if is_in_grid(*left):
+                    new_beams.add(left)
+                    beam_dimensions[left] += beam_dimensions[(bx, by)]
+                if is_in_grid(*right):
+                    new_beams.add(right)
+                    beam_dimensions[right] += beam_dimensions[(bx, by)]
+
+            elif is_in_grid(*down):
+                new_beams.add(down)
+                beam_dimensions[down] += beam_dimensions[(bx, by)]
+
+        beams = new_beams
+
+    return sum(d for (x, y), d in beam_dimensions.items() if y == height - 1)
+
+
+if __name__ == '__main__':
+    day = os.path.basename(__file__).replace('.py', '')
+    run(day, part_1, part_2)
